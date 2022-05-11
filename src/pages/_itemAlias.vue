82e09ef0b8170ed8aaf5cb6671e758c602ae5aec
Olga L <olla.bradford@gmail.com>
2022-05-11 14:49:40 +0300

    Tried to add redirect to 404 when there is no match with alias

--- src/pages/_itemAlias.vue
+++ src/pages/_itemAlias.vue
@@ -29,6 +29,27 @@ export default {
     const alias = this.$route.params.itemAlias
     const item = items.find(el => el.alias === alias)
     this.item = item
+
+    // this.item = this.checkAlias(items, alias)
+    // this.redirectToNotFound(item)
+    // console.log(this.item) //Не понимаю почему здесь после выполнения функции всегда undefined
+    //хотя если выводить шаги в функции, то она возвращает или объект el или null
+    //не понимаю, почему тогда item в data не становится тем, чем должен по результату функции
+  },
+  methods: {
+    checkAlias(items, alias) {
+      items.find(el => {
+        if (el.alias === alias) {
+          return el
+        }
+        return null
+      })
+    },
+    redirectToNotFound(item) {
+      if (item === null) {
+        this.$router.push({ name: '404' })
+      }
+    }
   }
 }
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,40 @@
-import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory
+} from 'vue-router'
 
-const routerHistory = createWebHashHistory();
+const routerHistory = createWebHashHistory()
 
-import HomePage from "./pages/home";
-import AboutPage from "./pages/about";
-import NotFoundPage from "./pages/notFound";
-import ItemAlias from "@/pages/_itemAlias";
+import HomePage from './pages/home'
+import AboutPage from './pages/about'
+import NotFoundPage from './pages/notFound'
+import ItemAlias from '@/pages/_itemAlias'
 
 const routers = createRouter({
-    history: routerHistory,
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomePage,
-        },
-        {
-            path: "/about",
-            name: "about",
-            component: AboutPage,
-        },
-        {
-            path: "/:itemAlias",
-            name: "itemAlias",
-            component: ItemAlias,
-        },
-        {
-            path: "/:CatchAll(.*)",
-            name: "404",
-            component: NotFoundPage,
-        },
-    ]
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage
+    },
+    {
+      path: '/:itemAlias',
+      name: 'itemAlias',
+      component: ItemAlias
+    },
+    {
+      path: '/:CatchAll(.*)',
+      name: '404',
+      component: NotFoundPage
+    }
+  ]
 })
 
-export default routers;
+export default routers
